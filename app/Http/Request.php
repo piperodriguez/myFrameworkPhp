80d782f7518e328f4b1913bfc8ae2e618d7dca86
frodriguez <vargasjuan367@gmail.com>
2020-10-25 19:38:53 -0500

    logica para implementar peticiones al controller

---
+++ app/Http/Request.php
@@ -0,0 +1,65 @@
+<?php 
+
+namespace App\Http;
+/**
+ * Clase encargada de realizar el proceso
+ * FrontController
+ * Recibe las peticiones que vienen por la URL
+ * @param $segmentos //ruta
+ * @param $ctr // controlador name
+ * @param $acc // metodo del controlador
+ * @author Juan Felipe Rodríguez
+ */
+class Request
+{
+    protected $segmentos = [];
+    protected $ctr;
+    protected $acc;
+    /**
+     * se recibe la petición
+     * 
+     */
+    public function __construct()
+    {
+        //suponiendo que el dominio fuera
+        //http://sitiofelipe/controlador/metodo
+        //convertimos en array la url de la peticion
+
+        $this->segmentos = explode('/', $_SERVER['REQUEST_URI']);
+        $this->setController();
+        $this->setMethod();
+    }
+
+    public function setController()
+    {
+        $this->ctr = empty($this->segmentos[1])
+                    ? 'home'
+                    : $this->segmentos[1];
+    }
+    public function setMethod()
+    {
+
+        $this->acc = empty($this->segmentos[2])
+            ? 'index'
+            : $this->segmentos[2];
+    }
+    public function getController()
+    {
+        $ctr = ucfirst($this->ctr);
+        return "App\Http\Controllers\\{$ctr}Controller";
+    }
+    public function getMethod()
+    {
+        return $this->acc;
+    }
+    public function send()
+    {
+        $ctr = $this->getController();
+        $acc = $this->getMethod();
+        $response = call_user_func([
+            new $ctr,
+            $acc
+        ]);
+        $response->send();
+    }
+}
---
+++ app/Http/Response.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http;
+
+class Response
+{
+    /**
+     * En el momento solo retornamos una vista
+     * pero mas adelante si es necesario desde aqui 
+     * se condifgura para que retorne
+     * array, json, pdf, excel, xml etc ...
+     */
+    protected $view;
+
+    public function __construct($view)
+    {
+        $this->view = $view;
+    }
+    public function getView()
+    {
+        return $this->view;
+    }
+    public function send()
+    {
+        $view = $this->getView();
+        $content = \file_get_contents(__DIR__ . "/../../views/$view.php");
+        require __DIR__ . "/../../views/layout.php";
+    }
+}
